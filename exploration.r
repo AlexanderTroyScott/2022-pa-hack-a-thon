81d45b19aa4b2e2b4a0ec442650f5012a86e4325
Alex <Alexander.Troy.Scott@gmail.com>
2022-05-12 18:18:11 -0500

    Update exploration.r
    Add function

--- exploration.r
+++ exploration.r
@@ -9,30 +9,25 @@ library(wordcloud2)
 install.packages("tm")
 library(tm)
 
+set.seed(1234)
+
+summarize_column <- function(field) {
+    docs <- Corpus(VectorSource(field))
+    docs <- docs %>%
+      #tm_map(removeNumbers) %>%
+      tm_map(removePunctuation) %>%
+      tm_map(stripWhitespace)
+    dtm <- TermDocumentMatrix(docs) 
+    matrix <- as.matrix(dtm) 
+    words <- sort(rowSums(matrix),decreasing=TRUE) 
+    df <- data.frame(word = names(words),freq=words)
+    #wordcloud(words = df$word, freq = df$freq, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35)
+    print(df)
+    }
+
 temp <- train
 temp <- temp %>% mutate_if(sapply(temp, is.character), as.factor)
-temp$Heating_Central <- grepl("gas",ignore.case = TRUE,temp$Heating)
-glimpse(temp)
-summary(temp)
 
-heating <- temp$Heating
-docs <- Corpus(VectorSource(heating))
-
-docs <- docs %>%
-  tm_map(removeNumbers) %>%
-  tm_map(removePunctuation) %>%
-  tm_map(stripWhitespace)
-docs <- tm_map(docs, content_transformer(tolower))
-docs <- tm_map(docs, removeWords, stopwords("english"))
-
-dtm <- TermDocumentMatrix(docs) 
-matrix <- as.matrix(dtm) 
-words <- sort(rowSums(matrix),decreasing=TRUE) 
-df <- data.frame(word = names(words),freq=words)
-
-set.seed(1234) # for reproducibility 
-wordcloud(words = df$word, freq = df$freq, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35)
-glimpse(df$word)
-
-df <- df %>% mutate_if(sapply(df, is.character), as.factor)
-print(df)
+summarize_column(temp$Cooling)
+summarize_column(temp$Laundry.features)
+summarize_column(temp$Appliances.included)
