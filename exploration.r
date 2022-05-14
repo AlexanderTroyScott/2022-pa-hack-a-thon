533f4c09094105208f82dec01a62318aec64d34c
Alex <Alexander.Troy.Scott@gmail.com>
2022-05-14 15:44:56 -0500

    Update exploration.r
    Added preliminary xgboost

--- exploration.r
+++ exploration.r
@@ -62,9 +62,26 @@ temp <- binarize_column(df=temp, col="Laundry.features")
 temp <- binarize_column(df=temp, col="Parking.features")
 temp <- binarize_column(df=temp, col="City")
 
-temp <- temp %>% select(-Summary)
-temp <- temp %>% clean_names()
+temp <- adv_train %>% clean_names() %>% mutate(log.sold = log(sold_price), log.list = log(listed_price), log.dif = log.sold-log.list, dol.dif = sold_price-listed_price)
+stat <- temp %>% summarise(n=n(),
+                           log.mean = mean(log.sold),
+                           log.dev = sd(log.sold), 
+                           log.median=median(log.sold), 
+                           dol.mean = mean(sold_price), 
+                           dol.dev = sd(sold_price),
+                           dol.median = median(sold_price),
+                           dif.mean = mean(dol.dif), 
+                           dif.dev = sd(dol.dif),
+                           dif.median = median(dol.dif)
+                          )
+temp3 <- data.frame(temp,stat) %>% filter(dol.dif>dif.mean+2*dif.dev)
 
+#Columns
+c('id','sold_price','summary','type','year_built','heating','cooling','parking','lot','bedrooms','bathrooms','full_bathrooms',
+  'total_interior_livable_area','total_spaces','garage_spaces','region','elementary_school','elementary_school_score',
+  'elementary_school_distance','middle_school','middle_school_score','middle_school_distance','high_school','high_school_score',
+  'high_school_distance','flooring','heating_features','cooling_features','appliances_included','laundry_features','parking_features',
+  'tax_assessed_value','annual_tax_amount','listed_on','listed_price','last_sold_on','last_sold_price','city','zip','state')
 
 model.rf <- randomForest(formula = sold_price ~ ., 
                          data = temp,
@@ -74,3 +91,29 @@ model.rf <- randomForest(formula = sold_price ~ .,
                          nodesize = 100, # The minimum number of observations in each leaf node of a tree - this controls complexity.
                          importance = TRUE
                          )
+
+
+#XGBoost
+temp <- adv_train %>% clean_names() %>% mutate(log.sold = log(sold_price), log.list = log(listed_price), log.dif = log.sold-log.list, dol.dif = sold_price-listed_price)
+stat <- temp %>% summarise(n=n(),
+                           log.mean = mean(log.sold),
+                           log.dev = sd(log.sold), 
+                           log.median=median(log.sold), 
+                           dol.mean = mean(sold_price), 
+                           dol.dev = sd(sold_price),
+                           dol.median = median(sold_price),
+                           dif.mean = mean(dol.dif), 
+                           dif.dev = sd(dol.dif),
+                           dif.median = median(dol.dif)
+                          )
+temp3 <- data.frame(temp,stat)
+
+boosted_data <- temp3 %>% select(all_of(predictors),log.dif) %>% select(-last_sold_price) %>% filter('id' != 35868) 
+boosted_data <- boosted_data %>% mutate_if(sapply(boosted_data, is.character), as.factor)
+summary(boosted_data)
+labels <- boosted_data$log.dif %>% as.numeric()-1
+train <- model.matrix(~.+0, data=boosted_data[,-c("log.dif"),with=F])
+dtrain <- xgb.DMatrix(data=train,label=labels)
+
+params <- list(booster = "gbtree", objective = "reg:squarederror", eta = 0.3, gamma=0, max_depth=5, min_child_weight=1, subsample=1, colsample_bytree=1)
+xgbcv <- xgb.cv( params = params, data = dtrain, nrounds = 100, nfold = 5, showsd = T, stratified = T, print_every_n = 10, early_stop_round = 20, maximize = F)
