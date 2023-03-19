1b03b432bb6cb9c79eaa6edb6ddaba549132b534
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-19 06:03:28 -0500

    Update explore_2022.sql

--- dbt/models/explore_2022.sql
+++ dbt/models/explore_2022.sql
@@ -1,6 +1,13 @@
+{{ config(
+    materialized='table',
+    schema='hackathons'
+) }}
+
 SELECT CAST('Year built' AS int)        as 'Year built'
   ,case
     when '"Sold Price"' is null then 'Yes'
     else 'No'
   end                                   as target
 FROM from {{ source('raw_data', 'advanced_2022') }}
+
+{{ alias('explore_2022') }}
