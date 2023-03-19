ae5eae8e163d6e6d4e76bfec6a5b429d68556378
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-19 04:19:20 -0500

    Update explore_2022.sql

--- dbt/models/explore_2022.sql
+++ dbt/models/explore_2022.sql
@@ -3,4 +3,4 @@ SELECT CAST('Year built' AS int)        as 'Year built'
     when '"Sold Price"' is null then 'Yes'
     else 'No'
   end                                   as target
-FROM username.public.advance_2022
+FROM advance_2022
