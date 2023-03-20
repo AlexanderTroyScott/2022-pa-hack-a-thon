9f98a4d1819f8048f6d048b5863ab1ba8d252b4e
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-20 11:06:26 -0500

    Update stg_2022_advanced.sql

--- dbt/models/staging/stg_2022_advanced.sql
+++ dbt/models/staging/stg_2022_advanced.sql
@@ -1,4 +1,5 @@
 select * from
 {{ dbt_utils.union_relations(
     relations=[source('raw_data', '2022_advanced_train'), source('raw_data', '2022_advanced_test')]
-) }}
+) }} as source,
+select * from source
