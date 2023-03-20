bae1bf9102de54239a9e77aeb6f4f94dac4707d3
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-20 11:20:51 -0500

    Update stg_2022_advanced.sql

--- dbt/models/staging/stg_2022_advanced.sql
+++ dbt/models/staging/stg_2022_advanced.sql
@@ -4,11 +4,6 @@
         alias = '2022_advanced'
     )
 }}
-
-with source as 
-( select * from
 {{ dbt_utils.union_relations(
     relations=[source('raw_data', '2022_advanced_train'), source('raw_data', '2022_advanced_test')]
-) }} as subquery
-)
-select * from source
+) }}
