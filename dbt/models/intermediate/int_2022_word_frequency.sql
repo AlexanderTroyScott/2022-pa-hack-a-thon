ae1703ca3af58aeadc7c1228d1f6e590f8505dad
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-21 18:43:41 -0500

    Update int_2022_word_frequency.sql

--- dbt/models/intermediate/int_2022_word_frequency.sql
+++ dbt/models/intermediate/int_2022_word_frequency.sql
@@ -2,7 +2,7 @@
 {{ config(alias='word_count_heating_feature') }}
 {{
     word_count(
-        source=ref('int_2022_cleaned_data'),
+        source=ref('hackathons.int_2022_cleaned_data'),
         column='heating_feature'
     )
 }}
