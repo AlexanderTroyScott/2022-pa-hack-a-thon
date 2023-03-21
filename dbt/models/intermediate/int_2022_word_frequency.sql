121d98e185ae8fe808f4b716af5fb766e402fbeb
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-21 18:24:56 -0500

    Update int_2022_word_frequency.sql

--- dbt/models/intermediate/int_2022_word_frequency.sql
+++ dbt/models/intermediate/int_2022_word_frequency.sql
@@ -1,25 +1,7 @@
-{{ config(materialized='table') }}
-
--- Select the unique words from the heating_features column
-WITH unique_words AS (
-  SELECT DISTINCT heating_features
-  FROM {{ ref('int_2022_cleaned_data') }}
-),
-
--- Split the heating_features column into individual words
-split_words AS (
-  SELECT heating_features, regexp_split_to_table(heating_features, ',\s*') AS word
-  FROM {{ ref('int_2022_cleaned_data') }}
-),
-
--- Count the frequency of each word
-word_counts AS (
-  SELECT word, COUNT(*) AS frequency
-  FROM split_words
-  GROUP BY word
-)
-
--- Final select statement that outputs the word frequency summary
-SELECT word, frequency
-FROM word_counts
-ORDER BY frequency DESC
+{{ dbt_utils.generate_alias('word_count', 'heating_feature') }}
+{{
+    word_count(
+        source=ref('int_2022_cleaned_data'),
+        column='heating_feature'
+    )
+}}
