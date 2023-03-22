4c3bd470f101a12f984105b4072c9884e45fa114
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-21 19:26:34 -0500

    Update int_2022_word_frequency.sql

--- dbt/models/intermediate/int_2022_word_frequency.sql
+++ dbt/models/intermediate/int_2022_word_frequency.sql
@@ -1,13 +1,4 @@
 WITH heating_word_counts AS (
   {{ word_count(source=ref('int_2022_cleaned_data'), column='heating_features') }}
-),
-cooling_word_counts AS (
-  {{ word_count(source=ref('int_2022_cleaned_data'), column='cooling_features') }}
-),
-parking_word_counts AS (
-  {{ word_count(source=ref('int_2022_cleaned_data'), column='parking_features') }}
 )
-SELECT *
-FROM heating_word_counts
-JOIN cooling_word_counts ON heating_word_counts.word = cooling_word_counts.word
-JOIN parking_word_counts ON heating_word_counts.word = parking_word_counts.word
+select * from heating_word_counts
