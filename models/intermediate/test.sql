a5c6e3d91affb51c15bd443417af5dd260378f00
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 14:25:40 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -21,7 +21,7 @@ value_columns AS (
   SELECT
     ARRAY_AGG(CASE WHEN LOWER(SPLIT_PART(my_column, ',', num)) = sv.value THEN 1 ELSE 0 END ORDER BY num) AS value_{{ sv.value }}
   FROM {{ ref('int_2023_data') }}
-  CROSS JOIN split_values sv
+  CROSS JOIN split_values as sv
   CROSS JOIN generate_series(1, 500) num -- Replace 100 with a larger number if needed
   GROUP BY my_column
 ),
