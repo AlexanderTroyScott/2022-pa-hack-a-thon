393472e6e64c2edda5bc27ca6cf92637859db2ee
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 15:14:54 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,5 +1,5 @@
 WITH source as (select * from {{ ref('int_2023_data') }})
 SELECT distinct
 
-    regexp_split_to_table(hashtags, ',') AS hashtag
+    LOWER(regexp_split_to_table(hashtags, ',')) AS hashtag
   FROM source
