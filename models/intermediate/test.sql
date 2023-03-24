e7963327d764406b67dd95fc84b2333b20661f8d
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 13:53:48 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -4,4 +4,4 @@ SELECT
   {% for value in run_query("SELECT DISTINCT UNNEST(string_to_array(hashtag, ',')) AS value FROM int_2023_data") %}
     CASE WHEN my_column LIKE '%{{ value.value }}%' THEN 1 ELSE 0 END AS {{ value.value|replace(' ', '_') }}
   {% endfor %}
-FROM my_table;
+FROM {{ ref('stg_2023_advanced') }};
