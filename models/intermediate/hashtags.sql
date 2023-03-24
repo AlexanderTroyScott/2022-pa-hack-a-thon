a82a52e82c5675a44d1cb385827999e261f40d6e
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 15:48:04 -0500

    Create hashtags.sql

---
+++ models/intermediate/hashtags.sql
@@ -0,0 +1,8 @@
+{% set hashtags = run_query("SELECT array_agg(hashtag) AS hashtag_array FROM (SELECT DISTINCT TRIM(LOWER(regexp_split_to_table(hashtags, ','))) AS hashtag FROM {{ ref('int_2023_data') }}) subquery").rows[0]['hashtag_array'] %}
+
+{% if hashtags %}
+{% for h in hashtags %}
+  EXECUTE 'ALTER TABLE int_2023_data ADD COLUMN hashtag_' || h || ' BOOLEAN DEFAULT FALSE';
+  UPDATE int_2023_data SET hashtag_' || h || ' = TRUE WHERE LOWER(hashtags) LIKE ''%' || h || '%''';
+{% endfor %}
+{% endif %}
