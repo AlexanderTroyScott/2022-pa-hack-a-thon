0f74933a147a1bd541947ff9a1db75ccb8000406
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 13:53:20 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,6 +1,7 @@
-{% set binary_data = 
-    macro('binarize_columns',
-          table='int_2023_data',
-          column='hashtag',
-          prefix='hashtag_') 
-%}
+CREATE TABLE my_table_binarized AS
+SELECT
+  *,
+  {% for value in run_query("SELECT DISTINCT UNNEST(string_to_array(hashtag, ',')) AS value FROM int_2023_data") %}
+    CASE WHEN my_column LIKE '%{{ value.value }}%' THEN 1 ELSE 0 END AS {{ value.value|replace(' ', '_') }}
+  {% endfor %}
+FROM my_table;
