3e6f89dff1f0555561b6df1bf3f5c87a4e59f1d4
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 13:54:39 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,7 +1,7 @@
 CREATE TABLE my_table_binarized AS
 SELECT
   *,
-  {% for value in run_query("SELECT DISTINCT UNNEST(string_to_array(hashtag, ',')) AS value FROM int_2023_data") %}
+  {% for value in run_query("SELECT DISTINCT UNNEST(string_to_array(hashtag, ',')) AS value FROM hackathons.int_2023_data") %}
     CASE WHEN my_column LIKE '%{{ value.value }}%' THEN 1 ELSE 0 END AS {{ value.value|replace(' ', '_') }}
   {% endfor %}
 FROM {{ ref('stg_2023_advanced') }};
