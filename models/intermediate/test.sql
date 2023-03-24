99f6c2b7db8c5895b84b7156f115fa4e9880234e
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 15:11:17 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,19 +1,5 @@
---with source as (select * from {{ ref('stg_2023_advanced') }})
---,
--- Define a macro to split a comma-separated string into an array of strings
-{% macro split_by_comma(column_name) %}
-  SELECT ARRAY_AGG(DISTINCT TRIM(LOWER(UNNEST(SPLIT({{ column_name }}, ','))))) AS {{ column_name }}_array
-{% endmacro %}
-
--- Use the macro to split the "hashtags" column and create a new table with binary columns for each unique hashtag
+WITH source as (select * from {{ ref(int_2023_data) }})
 SELECT 
-  *,
-  {% for hashtag in ref('int_2023_data') %} 
-    CASE WHEN '{{ hashtag }}' IN UNNEST(hashtags_array) THEN 1 ELSE 0 END AS {{ hashtag }}_binary{% if not loop.last %},{% endif %}
-  {% endfor %}
-FROM (
-  SELECT 
-    *,
-    {% raw %}{{ split_by_comma('hashtags') }}{% endraw %}
-  FROM {{ ref('int_2023_data') }}
-) subquery
+    id,
+    regexp_split_to_table(hashtags, ',') AS hashtag
+  FROM source
