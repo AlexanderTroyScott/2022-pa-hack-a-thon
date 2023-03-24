ef0084530bbc68956422c7789a031bb701dc043a
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 15:11:57 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,4 +1,4 @@
-WITH source as (select * from {{ ref(int_2023_data) }})
+WITH source as (select * from {{ ref('int_2023_data') }})
 SELECT 
     id,
     regexp_split_to_table(hashtags, ',') AS hashtag
