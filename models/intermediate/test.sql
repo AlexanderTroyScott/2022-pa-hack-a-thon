0d16911892d10bb20e677a2dce2699ca0f81be87
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 14:49:38 -0500

    Update test.sql

--- models/intermediate/test.sql
+++ models/intermediate/test.sql
@@ -1,5 +1,26 @@
-SELECT 
-    {% for value in ref('int_2023_data').column(hashtags).distinct().execute() %}
-      CASE WHEN '{{ value }}' IN ({{ ref('int_2023_data').column(hashtags).execute()|join("', '") }}') THEN 1 ELSE 0 END AS {{ hashtags }}_{{ value }},
-    {% endfor %}
-  FROM {{ ref('int_2023_data') }}
+-- define the source data
+{{source('hackathons', 'int_2023_data')}}
+
+-- define the macro to split and lowercase the column
+{% macro split_and_lowercase(column_name) %}
+  select distinct lower(trim(unnest(regexp_split_to_array({{ hashtags }}, ',')))) as value
+{% endmacro %}
+
+-- define the model to transform the data
+{{
+
+  config(
+    materialized='table',
+    unique_key='id',
+  )
+
+}}
+
+select
+  id,
+  lower(hackathons) as column_name,
+  {% for value in run_macro('split_and_lowercase', args=[ref('int_2023_data.hashtags')]) %}
+    case when lower(hashtags) like '%{{ value.value }}%' then 1 else 0 end as {{ value.value }}
+    {% if not loop.last %},{% endif %}
+  {% endfor %}
+from {{ ref('int_2023_data') }}
