e1c895f59089e723636bcff74c2d896fca75ee63
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 14:35:08 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -10,21 +10,3 @@ version: 2
   FROM {{ ref('int_2023_data') }}
   GROUP BY 1, 2
 {% endmacro %}
-
-# Define the model that uses the macro to binarize the column
-models:
-  - name: my_binarized_data
-    description: "Binarized version of my_source_data"
-    columns:
-      {% for value in ref('int_2023_data').column('hashtags').distinct().execute() %}
-        - name: hashtags_{{ value }}
-          description: "1 if my_column contains {{ value }}, 0 otherwise"
-          tests:
-            - not_null
-            - unique
-      {% endfor %}
-    pre-hook: |
-      DROP TABLE IF EXISTS {{ this }} CASCADE;
-    materialized: table
-    sql: |
-      {{ binarize_column('hashtags') }}
