8ca0fe91ccbaa426349156ced8d7222edc98bbfa
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-24 14:38:36 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -3,7 +3,7 @@ version: 2
 # Define the macro to split the column by comma and binarize the results
 {{- macro binarize_column(column_name) -}}
   SELECT 
-    {% for value in ref('my_source_data').column(column_name).distinct().execute() %}
+    {% for value in ref('int_2023_data').column(hashtags).distinct().execute() %}
       CASE WHEN '{{ value }}' IN ({{ ref('int_2023_data').column(hashtags).execute()|join("', '") }}') THEN 1 ELSE 0 END AS {{ hashtags }}_{{ value }},
     {% endfor %}
     {{ ref('int_2023_data').column(hashtags) }}
