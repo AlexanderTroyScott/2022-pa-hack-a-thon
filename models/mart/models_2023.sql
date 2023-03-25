5df1f7b69fa11f2be887ae8eb4a1b01dc6a83fb1
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 15:51:14 -0500

    Update models_2023.sql

--- models/mart/models_2023.sql
+++ models/mart/models_2023.sql
@@ -5,19 +5,19 @@
     )
 }}
 with source as ( 
-select source                   as source
-    ,screen_name              as screen_name
+select source               TEXT NOT NULL    as source
+    ,screen_name           TEXT NOT NULL    as screen_name
     ,created_at                 as created_at
  --   ,full_text                  as full_text
-    ,display_text_range         as display_text_range
-    ,in_reply_to_screen_name    as in_reply_to_screen_name
-    ,is_quote_status            as is_quote_status
-    ,includes_media             as includes_media
+    ,display_text_range      INT NOT NULL    as display_text_range
+    ,in_reply_to_screen_name  TEXT NOT NULL  as in_reply_to_screen_name
+    ,is_quote_status         BOOLEAN NOT NULL   as is_quote_status
+    ,includes_media          BOOLEAN NOT NULL   as includes_media
   --  ,hashtags                   as hashtags
-    ,user_mentions              as user_mentions
+    ,user_mentions         TEXT NOT NULL     as user_mentions
  --   ,urls                       as urls
-    ,emoji_rocket               as emoji_rocket
-    ,target                     as target
+    ,emoji_rocket         BOOLEAN NOT NULL      as emoji_rocket
+    ,target                INT NOT NULL     as target
 from 
 {{ ref('int_2023_data') }}
 )
