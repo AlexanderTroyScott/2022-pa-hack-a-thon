4f04e645b7f6619d64fc29e23d1f8744238f7b6b
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 07:00:41 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -5,15 +5,18 @@ WITH split_data AS (
     LOWER({{ column }}) AS {{ column }},
     REGEXP_SPLIT_TO_TABLE(LOWER({{ column }}), ',') AS split_column
   FROM {{ ref('int_2023_data') }}
-  where {{ column }} is not NULL
+  where {{ column }} is not NULL and tweet_id = 100001
+),
+
+unique_data AS (
+  SELECT DISTINCT split_column
+  FROM split_data
 ),
 
 binned_data AS (
   SELECT
     {{ column }}
-    {% for row in (SELECT DISTINCT split_column
-  FROM split_data
-) %}
+    {% for row in unique_data %}
       ,CASE
         WHEN {{ row.split_column }} IS NOT NULL THEN 1
         ELSE 0
@@ -21,8 +24,6 @@ binned_data AS (
     {% endfor %}
   FROM split_data
 )
-
-
 
 SELECT * from binned_data
 
