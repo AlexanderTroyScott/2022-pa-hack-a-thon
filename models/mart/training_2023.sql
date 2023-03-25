715054497e0f0a81cc548d2fbe331abeee2ba0b3
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 10:00:50 -0500

    Update training_2023.sql

--- models/mart/training_2023.sql
+++ models/mart/training_2023.sql
@@ -7,17 +7,18 @@
 with source as ( 
 select screen_name              as screen_name
     ,created_at                 as created_at
-    ,full_text                  as full_text
+ --   ,full_text                  as full_text
     ,display_text_range         as display_text_range
     ,in_reply_to_screen_name    as in_reply_to_screen_name
     ,is_quote_status            as is_quote_status
     ,includes_media             as includes_media
     ,hashtags                   as hashtags
     ,user_mentions              as user_mentions
-    ,urls                       as urls
+ --   ,urls                       as urls
     ,emoji_rocket               as emoji_rocket
     ,target                     as target
 from 
 {{ ref('int_2023_data') }}
+    where source = 'Train'
 )
 select * from source
