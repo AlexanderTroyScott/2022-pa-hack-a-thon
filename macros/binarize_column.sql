78341afff2305896e93a10af80349161ce00126e
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 06:35:37 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -1,31 +1,31 @@
 {% macro binarize_column(column, prefix) %}
-(
-  SELECT
-    {{ column }},
-    {% for row in (
-  SELECT DISTINCT split_column
-  FROM (
+
+WITH split_data AS (
   SELECT
     LOWER({{ column }}) AS {{ column }},
     REGEXP_SPLIT_TO_TABLE(LOWER({{ column }}), ',') AS split_column
   FROM {{ ref('int_2023_data') }}
-  where {{ column }} is not NULL
-)
-) %}
+),
+
+unique_data AS (
+  SELECT DISTINCT split_column
+  FROM split_data
+),
+
+binned_data AS (
+  SELECT
+    {{ column }},
+    {% for row in unique_data %}
       CASE
         WHEN {{ row.split_column }} IS NOT NULL THEN 1
         ELSE 0
       END AS {{ prefix }} || '_' || {{ row.split_column }}
       {% if not loop.last %},{% endif %}
     {% endfor %}
-  FROM (
-  SELECT
-    LOWER({{ column }}) AS {{ column }},
-    REGEXP_SPLIT_TO_TABLE(LOWER({{ column }}), ',') AS split_column
-  FROM {{ ref('int_2023_data') }}
-  where {{ column }} is not NULL
+  FROM split_data
 )
-)
- 
+
+SELECT *
+FROM binned_data;
 
 {% endmacro %}
