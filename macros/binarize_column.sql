af552b01864e8e3c633616052fc27c4efa0240e2
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 06:39:56 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -11,10 +11,22 @@ WITH split_data AS (
 unique_data AS (
   SELECT DISTINCT split_column
   FROM split_data
+),
+binned_data AS (
+  SELECT
+    {{ column }},
+    {% for row in unique_data %}
+      CASE
+        WHEN {{ row.split_column }} IS NOT NULL THEN 1
+        ELSE 0
+      END AS {{ prefix }} || '_' || {{ row.split_column }}
+      {% if not loop.last %},{% endif %}
+    {% endfor %}
+  FROM split_data
 )
 
 
-SELECT * from unique_data
+SELECT * from binned_data
 
 
 {% endmacro %}
