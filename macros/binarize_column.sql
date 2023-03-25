4acef862374c0d1c30a354d3c15c42fdd0829be8
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 08:42:38 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -4,7 +4,7 @@ WITH split_data AS (
   {% for element in ["bank_transfer", "credit_card", "gift_card"] %}
       ,1 AS {{ prefix }}_{{ element }}
     {% endfor %}
-  FROM (select tweet_id, '["' || array_agg(lower(REPLACE({{column}}, ' ', '')),'","') || '"]' as {{column}} from {{ ref('int_2023_data') }}) as subquery
+  FROM (select tweet_id, '["' || array_agg(string_split(lower(REPLACE({{column}}, ' ', '')),','),'","') || '"]' as {{column}} from {{ ref('int_2023_data') }}) as subquery
   where {{ column }} is not NULL and tweet_id between 100001 and 100010
 )
 select * from split_data
