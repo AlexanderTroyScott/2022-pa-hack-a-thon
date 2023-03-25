a358fa6890692184e1844d800c7873f6de6af12c
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 09:46:17 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -1,14 +1,19 @@
 {% macro binarize_column(column, prefix) %}
 
-{%- set hashtags = adapter.get_columns_in_relation(ref('int_2023_data')) -%}
-
-WITH split_data AS (
-  SELECT * 
-  {%- for element in hashtags -%}
-      ,1 AS {{ prefix }}_{{ element.name }}
-  {%- endfor -%}
-  FROM (select tweet_id, string_to_array(hashtags,',') as hashtags  from {{ ref('int_2023_data') }}) as subquery
-  where {{ column }} is not NULL and tweet_id between 100001 and 100010
+WITH hashtag_data AS (
+  SELECT tweet_id, STRING_TO_ARRAY(hashtags, ',') AS hashtags
+  FROM {{ ref('int_2023_data') }}
+  WHERE {{ column }} IS NOT NULL AND tweet_id BETWEEN 100001 AND 100010
+),
+split_data AS (
+  SELECT tweet_id, LOWER(UNNEST(hashtags)) AS hashtag
+  FROM hashtag_data
 )
-select * from split_data
+SELECT 
+  tweet_id, 
+  {% for element in ["category_a", "category_b", "category_c"] %}
+    CASE WHEN hashtag = '{{ element }}' THEN 1 ELSE 0 END AS {{ element }}
+    {% if not loop.last %},{% endif %}
+  {% endfor %}
+FROM split_data;
 {% endmacro %}
