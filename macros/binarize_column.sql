38f843b3d39ecb8242f13b9ca73b44694811c2ee
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 06:30:09 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -1,6 +1,6 @@
 {% macro binarize_column(column, prefix) %}
 
-WITH split_data AS (
+ (
   SELECT
     LOWER({{ column }}) AS {{ column }},
     REGEXP_SPLIT_TO_TABLE(LOWER({{ column }}), ',') AS split_column
