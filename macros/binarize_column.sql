72c0effde48f67fa6e1dc12bca9ffe83f2b0a703
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 07:42:47 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -4,13 +4,13 @@ with temp as
 WITH split_data AS (
   SELECT
     LOWER({{ column }}) AS {{ column }},
-    {% for element in unnest(REGEXP_SPLIT_TO_TABLE(trim(lower(hashtags)), ',')) %}
+    {% for element in unnest(REGEXP_SPLIT_TO_TABLE(hashtags, ',')) %}
       ,CASE
         WHEN {{ element }} IS NOT NULL THEN 1
         ELSE 0
       END AS {{ prefix }}_{{ element }}
     {% endfor %}
-  FROM {{ ref('int_2023_data') }}
+  FROM (select lower(REPLACE(hashtags, ' ', '')) from {{ ref('int_2023_data') }})
   where {{ column }} is not NULL and tweet_id between 100001 and 100010
 )
 {% endmacro %}
