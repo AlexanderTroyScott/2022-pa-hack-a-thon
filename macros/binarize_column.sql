9cda397849176c2916e643631059b13ee77b5cde
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 06:37:07 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -5,6 +5,7 @@ WITH split_data AS (
     LOWER({{ column }}) AS {{ column }},
     REGEXP_SPLIT_TO_TABLE(LOWER({{ column }}), ',') AS split_column
   FROM {{ ref('int_2023_data') }}
+  where {{ column }} is not NULL
 ),
 
 unique_data AS (
