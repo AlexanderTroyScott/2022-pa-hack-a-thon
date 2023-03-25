8c42cacfdc1d8b8fb6000d6635f353cb9c87d553
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 09:22:05 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -3,11 +3,11 @@
 
 WITH split_data AS (
   SELECT *
-  {% set array = string_to_array(hashtags) %}
+  {% set array = hashtags %}
   {% for element in  array %}
       ,1 AS {{ prefix }}_{{ element }}
   {% endfor %}
-  FROM (select tweet_id,  '["' || array_to_string(string_to_array(lower(replace({{column}},' ','')), ','),'","') || '"]' as {{column}} from {{ ref('int_2023_data') }}) as subquery
+  FROM (select tweet_id, string_to_array(hashtags) as hashtags  from {{ ref('int_2023_data') }}) as subquery
   where {{ column }} is not NULL and tweet_id between 100001 and 100010
 )
 select * from split_data
