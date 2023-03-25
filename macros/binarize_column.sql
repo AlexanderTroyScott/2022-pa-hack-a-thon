1d5ba24df3889a8cdcde1f069b2cefb5be07c7dc
Alex <Alexander.Troy.Scott@gmail.com>
2023-03-25 06:52:03 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -19,7 +19,7 @@ binned_data AS (
       CASE
         WHEN {{ row.split_column }} IS NOT NULL THEN 1
         ELSE 0
-      END AS {{ prefix }} || '_' || {{ row.split_column }}
+      END AS {{ prefix }} _ {{ row.split_column }}
       {% if not loop.last %},{% endif %}
     {% endfor %}
   FROM split_data
