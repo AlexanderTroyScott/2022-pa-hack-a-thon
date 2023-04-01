ecb42d95234b486a7d714176d5a4c48215096f55
Alex <Alexander.Troy.Scott@gmail.com>
2023-04-01 13:09:27 -0500

    Update binarize_column.sql

--- macros/binarize_column.sql
+++ macros/binarize_column.sql
@@ -1,17 +1,24 @@
-{% macro binarize_column(column, prefix) %}
+{% macro binarize_column(table_name, column, prefix, start_id, end_id) %}
+
+{% set table_name = table_name | ref %}
+
 WITH hashtag_data AS (
-  SELECT tweet_id, STRING_TO_ARRAY(hashtags, ',') AS hashtags
-  FROM {{ ref('int_2023_data') }}
-  WHERE {{ column }} IS NOT NULL AND tweet_id BETWEEN 100001 AND 100010
+  SELECT tweet_id, STRING_TO_ARRAY({{ column }}, ',') AS hashtags
+  FROM {{ table_name }}
+  WHERE {{ column }} IS NOT NULL AND tweet_id BETWEEN {{ start_id }} AND {{ end_id }}
 ),
 unique_hashtags AS (
   SELECT DISTINCT LOWER(UNNEST(hashtags)) AS hashtag
   FROM hashtag_data
 )
+
+{% set results = run_query('SELECT * FROM unique_hashtags') %}
+{% set unique_hashtags = results.columns[0].values() %}
+
 SELECT
   tweet_id
-  {% for row in unique_hashtags %}
-    ,CASE WHEN '{{ row.hashtag }}' = ANY(hashtags) THEN 1 ELSE 0 END AS {{ row.hashtag }}
+  {% for hashtag in unique_hashtags %}
+    ,CASE WHEN '{{ hashtag }}' = ANY(hashtags) THEN 1 ELSE 0 END AS {{ prefix }}_{{ hashtag }}
   {% endfor %}
 FROM hashtag_data
 
